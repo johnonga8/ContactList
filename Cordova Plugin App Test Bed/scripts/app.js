624c2c3d6b3e7e3da5c791d50304a2c2a061ad74
John Ong <john.ong@a8.sg>
2015-06-17 09:36:29 +0000

    Changes made by John Ong (john.ong@a8.sg).

--- Cordova Plugin App Test Bed/scripts/app.js
+++ Cordova Plugin App Test Bed/scripts/app.js
@@ -17,5 +17,20 @@
     	if(c.name.givenName && c.name.familyName) return c.name.givenName + " " + c.name.familyName;
     	return "No Name Listed";
     }
-
+	
+    window.getAllContacts = function(){
+        var options = new ContactFindOptions();
+        options.filter = "";
+        options.multiple = true;
+        var fields = ["*"];
+        navigator.contacts.find(fields, onContactSuccess, onError, options);
+    }
+    function onContactSuccess(contacts){
+        var template = kendo.template($("#contacts-template").html());
+        var dataSource = new kendo.data.DataSource({data:contacts});
+        dataSource.bind("change",function(){
+            $("#contacts-list").html(kendo.render(template, dataSource.view()));
+        });
+        dataSource.read();
+    }
 }());
